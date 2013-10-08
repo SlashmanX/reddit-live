40f93e9dbc33bfa61bc1210a5f5d63f8547cb686
Eóin Martin <eoin.martin.3007@gmail.com>
2013-10-08 22:19:41 +0100

    Added Reddit oAuth login

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ var browserijade = require('browserijade');
 var connect = require('connect');
 var MemoryStore = express.session.MemoryStore;
 var sessionStore = new MemoryStore();
+var passport = require('passport');
+var RedditStrategy = require('passport-reddit').Strategy;
+var crypto = require('crypto')
+
+var REDDIT_CONSUMER_KEY = "4b0oH04c0BHiOw";
+var REDDIT_CONSUMER_SECRET = "YpPpKwxnhm4Rqlvq4lSTvikItAE";
 
 
 var app = express();
@@ -55,6 +61,26 @@ io.on('connection', function (socket) {
     // socket.io will leave the room upon disconnect
 });
 
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new RedditStrategy({
+        clientID: REDDIT_CONSUMER_KEY,
+        clientSecret: REDDIT_CONSUMER_SECRET,
+        callbackURL: "http://localhost:3000/auth/reddit/callback/"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+));
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -68,8 +94,10 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser())
-app.use(express.session({store: sessionStore, secret: 'monkey', key: 'express.sid'}))
+app.use(express.cookieParser());
+app.use(express.session({store: sessionStore, secret: 'monkey', key: 'express.sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public', compress: true, optimization: 2 }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -82,18 +110,51 @@ if ('development' == app.get('env')) {
 app.get('/', function(req, res) {
     getActiveThreads(function(err, data) {
         var active = data.data.data;
-        res.render('home', {title: 'Home', activeThreads: active});
+        console.log(req.user);
+        res.render('home', {title: 'Home', activeThreads: active, user: req.user });
     })
 });
 app.get('/r/:subreddit/', function(req, res) {
-    res.render('stream', { title: req.params.subreddit, subreddit: req.params.subreddit });
-})
+    res.render('stream', { title: req.params.subreddit, subreddit: req.params.subreddit, user: req.user });
+});
+
 app.get('/r/:subreddit/comments/:topicid/:topicname?/*', function(req, res) {
     if(req.params.topicid.substring(0,3) == 't3_')
         res.redirect('/r/'+ req.params.subreddit+'/comments/'+ req.params.topicid.replace('t3_', '')+'/');
     else
-        res.render('stream', { title: req.params.subreddit, topic: req.params.topicid });
-})
+        res.render('stream', { title: req.params.subreddit, topic: req.params.topicid, user: req.user });
+});
+
+app.get('/auth/reddit/', function(req, res, next){
+    req.session.state = crypto.randomBytes(32).toString('hex');
+    passport.authenticate('reddit', {
+        state: req.session.state,
+        duration: 'permanent',
+        scope: 'identity,mysubreddits,submit,vote,read'
+    })(req, res, next);
+});
+
+app.get('/auth/reddit/callback/', function(req, res, next){
+    // Check for origin via state token
+    if (req.query.state == req.session.state){
+        passport.authenticate('reddit', {
+            successRedirect: '/',
+            failureRedirect: '/login/'
+        })(req, res, next);
+    }
+    else {
+        next( new Error(403) );
+    }
+});
+
+app.get('/login/', function(req, res){
+    res.render('login', { user: req.user });
+});
+
+app.get('/logout/', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
 var parser = JSONStream.parse() //emit parts that match this path (any element of the rows array)
 var req = request({url: 'http://stream.redditanalytics.com'})
